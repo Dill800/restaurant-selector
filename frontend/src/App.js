7e628555cc50fb9c3466a60317e77e81615d3b5d
Ryan Ahmed <ra700@hotmail.com>
2020-02-01 01:11:00 -0500

    Ryan com 3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,8 +5,24 @@ import './App.css';
 import Main from './main.js'
 
 const fontSize2 = {
-  fontSize: '32px'
+  fontSize: '64px',
+  color: '#0099cc',
+  backgroundColor: 'lightblue'
 };
+
+const babyBloo = {
+ 
+  backgroundColor: 'lightblue'
+};
+
+const createdBy = {
+  fontSize: '15px',
+  color: '#0099cc'
+};
+
+const contactUs = {
+  color: '#007399'
+}
 
 
 function getLat() {
@@ -24,18 +40,22 @@ function getLat() {
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
+      <header className="App-header" style= {babyBloo}>
+  
         <p style = {fontSize2}>SwampHacks Restaurant Finder</p>
         <img src={borg} className="App-logo" alt="logo" />
-        <p>
-          Created by: Ryan Ahmed, Brock Major, Dillon McGovern, Gary Wu, Daniel Shin, Rohan Samanta
+        <p style = {createdBy}>
+          <div >
+            <p></p>
+          </div>
+          Created by: Ryan Ahmed, Brock Major, Dillon McGovern, Gary Wu, Daniel Shin, Rohan Samanta, David Wang
         </p>
         <a
           className="App-link"
           href="https://dillonmcgovern.com/"
           target="_blank"
           rel="noopener noreferrer"
+          style = {contactUs}
         >
           Contact Us
         </a>
